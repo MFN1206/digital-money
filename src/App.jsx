38f0eb449e86302e99e924c4639b67cf5c2d688e
Gabriel Angelo <gabrielangelolira@gmail.com>
2025-06-02 18:28:50 -0300

    atualizado com a aula 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { EnvelopeSimple } from "phosphor-react";
+import { EnvelopeSimple, LockSimple } from "phosphor-react";
 import ImageLogin from "./assets/secure-login-animate.svg";
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
       </div>
 
       <div
-        className="bg-white w-1/2 h-full text-center flex justify-center items-center flex-col"
+        className="bg-white w-1/2 h-full text-center flex justify-center items-center flex-col gap-4"
         name="direita"
       >
         <h1 className="text-3xl font-bold">Welcome back</h1>
@@ -26,10 +26,10 @@ function App() {
           Enter your details to sign in to your account
         </p>
 
-        <form className="w-full flex flex-col items-center justify-center">
+        <form className="w-full flex flex-col items-center justify-center gap-4">
           <div className="flex flex-col text-left w-3/5">
             <label htmlFor="email">Email</label>
-            <label htmlFor="email" className="border border-gray-300 px-4 py-1 rounded-md flex items-center gap-2 focus-within:border-blue-500">
+            <label htmlFor="email" className="border-2 border-gray-300 px-4 py-1 rounded-md flex items-center gap-2 focus-within:border-blue-500">
               <EnvelopeSimple size={20} className="text-gray-400" />
               <input
                 type="email"
@@ -43,14 +43,22 @@ function App() {
 
           <div className="flex flex-col text-left w-3/5">
             <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className="border border-gray-300 px-4 py-1 rounded-md"
-              placeholder="*********"
-            />
+            <label htmlFor="password" className="border-2 border-gray-300 px-4 py-1 rounded-md flex items-center gap-2 focus-within:border-blue-500">
+              <LockSimple size={20} className="text-gray-400" />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="outline-none w-full"
+                placeholder="********"
+              />
+            </label>
           </div>
+
+          <button className="text-white bg-blue-700 w-3/5 py-2 rounded-md cursor-pointer hover:bg-blue-800 hover:transform hover:scale-105 duration-300">
+            Sign In
+          </button>
+
         </form>
       </div>
     </div>
