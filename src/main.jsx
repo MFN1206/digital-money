e591eea7ed28b1adbc4613a4fbed85b20b699c22
Gabriel Angelo <gabrielangelolira@gmail.com>
2025-06-30 18:30:34 -0300

    init components

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import LoginPage from './pages/LoginPage.jsx'
 import { BrowserRouter, Route, Routes } from "react-router";
+import DashboardPage from './pages/DashboardPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LoginPage />} />
+        <Route path='/dashboard' element={<DashboardPage />} index />
         <Route path='*' element={<h1>404 - Not Found</h1>} />
       </Routes>
     </BrowserRouter>
